994cb6c6baf7e2e98f1eec860a6b582508a36bd3
CouchPtato <singhsomay467@gmail.com>
2026-01-22 18:13:16 +0530

    better readability

--- parser.py
+++ parser.py
@@ -1,52 +1,78 @@
 from lexer import t_int, t_plus, t_minus
 
 class NumberNode:
-    def __init__(self, tok):
-        self.tok = tok
+    def __init__(self, number_token):
+        self.number_token = number_token
 
     def __repr__(self):
-        return f'{self.tok}'
+        return f"{self.number_token}"
 
 
-class BinOpNode:
-    def __init__(self, left_node, op_tok, right_node):
+class BinaryOperationNode:
+    def __init__(self, left_node, operator_token, right_node):
         self.left_node = left_node
-        self.op_tok = op_tok
+        self.operator_token = operator_token
         self.right_node = right_node
 
     def __repr__(self):
-        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
-    
+        return f"({self.left_node} {self.operator_token} {self.right_node})"
+
+
 class Parser:
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.tok_idx = -1
-        self.current_tok = None
-        self.advance()
+    def __init__(self, list_of_tokens):
+        self.tokens = list_of_tokens
+        self.current_token_index = -1
+        self.current_token = None
+        self.move_to_next_token()
 
-    def advance(self):
-        self.tok_idx += 1
-        if self.tok_idx < len(self.tokens):
-            self.current_tok = self.tokens[self.tok_idx]
-        return self.current_tok
+    # Move pointer to next token
+    def move_to_next_token(self):
+        self.current_token_index += 1
 
+        if self.current_token_index < len(self.tokens):
+            self.current_token = self.tokens[self.current_token_index]
+        else:
+            self.current_token = None
+
+        return self.current_token
+
+    # Entry point of parser
     def parse(self):
-        return self.expr()
+        return self.parse_expression()
 
-    def expr(self):
-        left = self.term()
+    # expression -> number ((+ or -) number)*
+    def parse_expression(self):
 
-        while self.current_tok is not None and self.current_tok.type in (t_plus, t_minus):
-            op_tok = self.current_tok
-            self.advance()
-            right = self.term()
-            left = BinOpNode(left, op_tok, right)
+        # First number
+        left_node = self.parse_number()
 
-        return left
+        # While we see + or -
+        while (
+            self.current_token is not None
+            and self.current_token.type in (t_plus, t_minus)
+        ):
 
-    def term(self):
-        tok = self.current_tok
+            operator_token = self.current_token
+            self.move_to_next_token()
 
-        if tok.type == t_int:
-            self.advance()
-            return NumberNode(tok)
+            right_node = self.parse_number()
+
+            # Build tree node
+            left_node = BinaryOperationNode(
+                left_node,
+                operator_token,
+                right_node
+            )
+
+        return left_node
+
+    # number → INT
+    def parse_number(self):
+
+        token = self.current_token
+
+        if token.type == t_int:
+            self.move_to_next_token()
+            return NumberNode(token)
+
+        raise Exception("Expected a number")
